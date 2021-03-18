a52197e00a39bb22cd1e1240d8964b8af26d4b84
Jonathan-zihindula <jonathanzihindula95@gmail.com>
2021-03-18 18:41:35 +0200

    update my gallery

--- gal.js
+++ gal.js
@@ -1,21 +1,37 @@
 
 var i = "";
- const  tabIndex = ['1"', '2"', '3"', '4"'];
+ const  tabIndex = ['1', '2', '3', '4'];
   
  for (i = 0; i < tabIndex.length; i++) {
     tabIndex[i];
-
-var photobulder = {
-        tag: '<img src="imagesecondaire',
-        alternative: 'image',
-        finTag: '/>',
-     
-    construct:
-        function () {
-            return this.tag + tabIndex[i] + ' alt=' + '"' + this.alternative + '"' + this.finTag;
-        },       
-    }
-   
-     console.log(photobulder.construct());
-     
  }
+var photobulder = {
+    tag: '<img src="imagesecondaire',
+    construct:
+    function () {
+        return this.tag;
+    },       
+}
+var photobulder2 = {
+    format: '.jpg"',
+    alternative: ' alt="image"',
+    finTag: '/>',
+    construct2: 
+        function() {
+            return this.format + this.alternative + this.finTag;
+        }
+    }
+
+
+console.log(photobulder.construct());
+console.log(photobulder2.construct2());
+
+
+var divContainer = document.querySelector('#flex');
+divContainer.innerHTML =
+    '<div class="photos">' + photobulder.construct() + tabIndex[0] + photobulder2.construct2() + '</div>' +
+    '<div class="photos">' + photobulder.construct() + tabIndex[1] + photobulder2.construct2() + '</div>' +
+    '<div class="photos">' + photobulder.construct() + tabIndex[2] + photobulder2.construct2() + '</div>';
+
+
+
